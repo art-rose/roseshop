3ced013407627c8cc14200e1fd65c70db0e06656
art-rose <mahdikhazri2021@mmmtunisie.com>
2025-08-18 05:25:40 -0700

    Delete includes directory

--- includes/auth.php
+++
@@ -1,3 +0,0 @@
-auth_php = """<?php require_once DIR.'/../config.php';
-
-function require_admin() { if (empty($_SESSION['admin'])) { header('Location: /admin/login.php'); exit; } } ?> """ open(os.path.join(base,"includes","auth.php"),"w",encoding="utf-8").write(auth_php)
